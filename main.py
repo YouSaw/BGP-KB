19e94f24a9ccc19f976aa803864c367ca36ab2a2
see <5see@informatik.uni-hamburg.de>
2018-07-14 12:37:21 +0200

    added argparse

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ import bgpBuilder as bgpb
 import log
 
 if __name__ == '__main__':
+    global session_name
+    global db_name
+
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", help="startTime")
-    parser.add_argument("-e", help="endTime")
-    parser.add_argument("-c", help="chunks")
-    parser.add_argument("-db", help="DB name")
-    parser.add_argument("-se", help="Session name")
+    parser.add_argument("-s", "--start", help="startTime")
+    parser.add_argument("-e", "--end", help="endTime")
+    parser.add_argument("-c", "--chunks", help="chunks")
+    parser.add_argument("-db", "--database", help="DB name")
+    parser.add_argument("-se", "--session", help="Session name")
+    args = parser.parse_args()
+
+    startTime = int(args.start)
+    endTime = int(args.end)
+    chunks = int(args.chunks)
+
     memoryDB = initDB()
-    bgpb.build_sql_db(collectos, start_time=1530063631, end_time=1531563631, memoryDB=memoryDB, chunks=4)
+    bgpb.build_sql_db(collectos, start_time=startTime, end_time=endTime, memoryDB=memoryDB, chunks=chunks)
     saveDB(db_name)
     log.rootLogger.info("[!] all done")
